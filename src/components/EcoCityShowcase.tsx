96db23870bfaff4a3a1fa945726c06c554a0019d
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-05-30 16:48:49 -0300

    Update EcoCityShowcase.tsx

--- src/components/EcoCityShowcase.tsx
+++ src/components/EcoCityShowcase.tsx
@@ -56,7 +56,7 @@ const EcoCityShowcase = () => {
               <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/8UkoWFrvot0?autoplay=1&mute=1&loop=1&playlist=8UkoWFrvot06"
+                src="https://youtu.be/khqovYBbZVU?si=CVFdZpg60NmNPZeB"
                 title="EcoCity - Iniciativa Privada"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
